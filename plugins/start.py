992b7a050d9ff89dfdbca08f39d39bbbbd51564b
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 01:31:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ async def start(client, message):
         [InlineKeyboardButton("Channel", url="https://t.me/Cat_Telegram_Projects")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/Cat_Telegram_Project_Club")]
-    ]
+    ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n https://telegra.ph/About-Yourtube-Bot-06-24"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
