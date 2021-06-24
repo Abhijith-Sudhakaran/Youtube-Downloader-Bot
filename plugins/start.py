21d30fbd77dbe7d1fcce5dd0afbed730c72c3eab
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-24 23:33:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/Cat_Telegram_Projects")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/Cat_Telegram_Project_Club")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I am the bot which allow to download videos from Youtube😜.use /help for More info and Usage.__Created by [CAT](https://t.me/Cat_of_telegramx)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
