2d38de954b048ff6f0e9c7fe0339a835c281d61c
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 00:52:33 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url...Join @Cat_Telegram_Project_Club to Support😊."
+    helptxt = f"Hey.I am Currently Support Single Youtube Links.(Playlist Added Soon)</b>\n I Am Currently Running in Pyrogram.Sometime speed become slowed down due to heavy Traffic.</b>\n **My Master is** @Cat_of_TelegramX .</b>\n If You Have Any Doubts Contact in @Cat_Telegram_Project_Club 🤗.</b>\n Thank You For Using Our Services💟."
     await message.reply_text(helptxt)
