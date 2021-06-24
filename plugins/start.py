08ffd4eb457c6b9738245742e5599afabf54a6e2
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 01:30:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/Cat_Telegram_Project_Club")]
     ]
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I am the bot which allow to download videos & Audios from Youtube😜 with High Quality.use /help for More info.Send me your Youtube link First😊. Select Your Quality Of Video or Audio😊. **Note - If You Select High Quality File.Its normally Take some more Time to Upload😊** .Thank You For using Our service💟... Type /help To Get Some Informations About me..."
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n https://telegra.ph/About-Yourtube-Bot-06-24"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
