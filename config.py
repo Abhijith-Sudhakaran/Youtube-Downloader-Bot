cbb3c6401c094b61e48dc2fb4185b1d31ec25332
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 01:44:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("1816351657:AAGzHQIE7EcpqVryYyHyXJTexRg4RocdMoQ")
+APP_ID = int(os.environ.get("2195622"))
+API_HASH = os.environ.get("fe9031812730b93d59b43a33b1db17ca")
 
-youtube_next_fetch = 0  # time in minute
+youtube_next_fetch = 1 # time in minute
 
 
 EDIT_TIME = 5
