fda26a2005191c27b8b880c04b4206af6b47dc42
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-11 09:35:49 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Hey <b>{message.from_user.first_name}</b>\n I am Currently Support Single Youtube Links.(Playlist Added Soon)</b>\n I Am Currently Running in Pyrogram.Sometime speed become slowed down due to heavy Traffic.</b>\n **My Master is** @Cat_of_TelegramX .</b>\n If You Have Any Doubts Contact in @Cat_Telegram_Project_Club 🤗.</b>\n Thank You For Using Our Services💟."
+    helptxt = f"Hey <b>{message.from_user.first_name}</b>\n I am Currently Support Single Youtube Links.(Playlist Added Soon)</b>\n I Am Currently Running in Pyrogram.Sometime speed become slowed down due to heavy Traffic.</b>\n **My Master is** @Telecat_X .</b>\n Join our Update's Channel now! @CatX_botx.</b>\n Thank You For Using Our Services 💟."
     await message.reply_text(helptxt)
