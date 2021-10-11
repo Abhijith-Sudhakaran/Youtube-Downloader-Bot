285abe3a8c314a82df0997eae2ec655c19244a66
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-11 09:57:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(Filters.command(["start"]), group=-2)
+@Client.on_message(Filters.command(["start, start@Xyoutube_bot"]), group=-2)
 async def start(client, message):
     # return
     Lasiya = InlineKeyboardMarkup([
@@ -13,5 +13,5 @@ async def start(client, message):
             "Updates ⚙",url="https://t.me/CatX_botz")]
     ])
     thumbnail_url = "https://telegra.ph/file/a488092b0f602ae43bbf0.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"Hellow<b>{message.from_user.first_name}</b>\n\n **• I am a Simple Youtube Downloader Bot. I can Download your Youtube Videos as Fast as I can.**\n\n ! - Sometime Downloading Became slowed down due to Heavy traffic in Server.\n•Join Update Channel Must for New features!.\n\n **Powered by** @CatX_botz 💚", reply_markup=Lasiya)
+    await message.reply_photo(thumbnail_url, caption=f"Hellow<b>{message.from_user.first_name}</b>\n\n **• I am a Simple Youtube Downloader Bot. I can Download your Youtube Videos as Fast as I can.**\n\n ! - Sometime Downloading Became slowed down due to Heavy traffic in Server.\n•Join Update Channel Must for New features!.\n\n For any Doubts click /help \n\n **Powered by** @CatX_botz 💚", reply_markup=Lasiya)
     raise StopPropagation
